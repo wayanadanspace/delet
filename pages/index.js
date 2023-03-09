39dc4896a2a5696de080184def727d830026a474
wayanadan <wayanadan.space@gmail.com>
2023-03-09 12:50:20 +0000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,51 @@
-import Link from 'next/link'
+import Layout from "../compenents/Layout";
+import Post from "../compenents/Post";
+import prisma from "../lib/prisma";
 
-export default function IndexPage() {
+const Blog = (props) => {
   return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </div>
-  )
-}
+    <Layout>
+      <div className="page">
+        <h1>My Blog</h1>
+        <main>
+          {props.feed.map((post) => (
+            <div key={post.id} className="post">
+              <Post post={post} />
+            </div>
+          ))}
+        </main>
+      </div>
+      <style jsx>{`
+        .post {
+          background: white;
+          transition: box-shadow 0.1s ease-in;
+        }
+        .post:hover {
+          box-shadow: 1px 1px 3px #aaa;
+        }
+        .post + .post {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true },
+  });
+
+  const serializedFeed = feed.map((post) => ({
+    ...post,
+    createdAt: post.createdAt.toISOString(),
+    updatedAt: post.updatedAt.toISOString(),
+  }));
+
+  return {
+    props: { feed: serializedFeed },
+  };
+};
+
+export default Blog;
